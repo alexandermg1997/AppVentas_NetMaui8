0995d14805677b76e6912bc6ff6c580e3c8b53e2
Erasmo Alexander Marín Gómez <shadowhunterszony@gmail.com>
2025-02-26 22:45:31 -0700

    First commit stable

---
+++ AppVenta/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using AppVenta.Pages;
+using Microsoft.Maui.Storage;
+namespace AppVenta
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+
+            var logueado = Preferences.Get("logueado", string.Empty);
+            if (string.IsNullOrEmpty(logueado))
+            {
+                MainPage = new LoginPage();
+            }
+            else
+            {
+                MainPage = new AppShell();
+            }
+
+        }
+    }
+}
---
+++ AppVenta/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,11 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace AppVenta
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
